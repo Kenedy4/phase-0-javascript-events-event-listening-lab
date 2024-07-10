16dd81498c6a7c7ea395e2dcdfeb3cd39fd13092
kenedy4 <kennedymusyoki8@gmail.com>
2024-07-10 20:39:46 +0300

    Fixed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+  //   addingEventListener();
+  const input = document.getElementById("button");
+
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+
+  input.addEventListener("click", clickAlert);
 }
